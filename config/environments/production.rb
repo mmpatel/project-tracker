724f35691b66b3421b951a32c5d6e7068494c54a
Mukesh <mpatel.ror@gmail.com>
2014-12-18 18:37:00 +0530

    add smpt settings to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,18 @@ Projecttracker::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   config.action_mailer.default_url_options = { :host => 'prodev-tracker.herokuapp.com' }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'my_app.com',
+    user_name:            "projecttracker007@gmail.com",
+    password:             "projecttracker12345",
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
