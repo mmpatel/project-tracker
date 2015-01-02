8a7c222f21cb7bd5059dea4def8501fcf05dc351
Mukesh <mpatel.ror@gmail.com>
2015-01-02 16:21:22 +0530

    update the admin email

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module Projecttracker
   class Application < Rails::Application
-    config.admins = %w{projecttracker007@gmail.com,petoskey@mobizard.com}
+    config.admins = %w{petoskey@mobizard.com projecttracker007@gmail.com}
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
